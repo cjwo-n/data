7ef2d37b0b0480e01381a51f01b7a5b590c6e9bc
cjwo-n <whwjddnjs0@naver.com>
2023-08-09 21:17:46 +0900

    0809 second commit

--- src/main/java/com/study/springboot/dto/MemberDTO.java
+++ src/main/java/com/study/springboot/dto/MemberDTO.java
@@ -1,33 +1,40 @@
 package com.study.springboot.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class MemberDTO {
 
 	//data 어노테이션 및 mapper어노테이션 없으니 get, set 정의
 	private String name;
 	private String email;
 	private String organization;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getOrganization() {
-		return organization;
-	}
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
-	@Override
-	public String toString() {
-		return "MemberDTO {name=" + name 
-				+ ", email=" + email 
-				+ ", organization=" + organization + "}";
-	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	public String getOrganization() {
+//		return organization;
+//	}
+//	public void setOrganization(String organization) {
+//		this.organization = organization;
+//	}
+//	@Override
+//	public String toString() {
+//		return "MemberDTO {name=" + name 
+//				+ ", email=" + email 
+//				+ ", organization=" + organization + "}";
+//	}
 }
